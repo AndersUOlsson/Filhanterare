4f82886f05de015e21f65c56c1a611e9f2c510d5
AndersUOlsson <olsson.anders@protonmail.com>
2019-01-28 15:09:09 +0100

    Add project files.

---
+++ Filhanterare/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Filhanterare
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SaveBtn = new System.Windows.Forms.Button();
+            this.SaveAsBtn = new System.Windows.Forms.Button();
+            this.richTextBoxWindow = new System.Windows.Forms.RichTextBox();
+            this.OpenFileBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // SaveBtn
+            // 
+            this.SaveBtn.Location = new System.Drawing.Point(13, 13);
+            this.SaveBtn.Name = "SaveBtn";
+            this.SaveBtn.Size = new System.Drawing.Size(91, 27);
+            this.SaveBtn.TabIndex = 0;
+            this.SaveBtn.Text = "Save";
+            this.SaveBtn.UseVisualStyleBackColor = true;
+            this.SaveBtn.Click += new System.EventHandler(this.SaveBtn_Click);
+            // 
+            // SaveAsBtn
+            // 
+            this.SaveAsBtn.Location = new System.Drawing.Point(110, 13);
+            this.SaveAsBtn.Name = "SaveAsBtn";
+            this.SaveAsBtn.Size = new System.Drawing.Size(91, 27);
+            this.SaveAsBtn.TabIndex = 1;
+            this.SaveAsBtn.Text = "Save As...";
+            this.SaveAsBtn.UseVisualStyleBackColor = true;
+            this.SaveAsBtn.Click += new System.EventHandler(this.SaveAsBtn_Click);
+            // 
+            // richTextBoxWindow
+            // 
+            this.richTextBoxWindow.Location = new System.Drawing.Point(13, 66);
+            this.richTextBoxWindow.Name = "richTextBoxWindow";
+            this.richTextBoxWindow.Size = new System.Drawing.Size(775, 372);
+            this.richTextBoxWindow.TabIndex = 2;
+            this.richTextBoxWindow.Text = "";
+            // 
+            // OpenFileBtn
+            // 
+            this.OpenFileBtn.Location = new System.Drawing.Point(697, 13);
+            this.OpenFileBtn.Name = "OpenFileBtn";
+            this.OpenFileBtn.Size = new System.Drawing.Size(91, 27);
+            this.OpenFileBtn.TabIndex = 3;
+            this.OpenFileBtn.Text = "Open File";
+            this.OpenFileBtn.UseVisualStyleBackColor = true;
+            this.OpenFileBtn.Click += new System.EventHandler(this.OpenFileBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.OpenFileBtn);
+            this.Controls.Add(this.richTextBoxWindow);
+            this.Controls.Add(this.SaveAsBtn);
+            this.Controls.Add(this.SaveBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SaveBtn;
+        private System.Windows.Forms.Button SaveAsBtn;
+        private System.Windows.Forms.RichTextBox richTextBoxWindow;
+        private System.Windows.Forms.Button OpenFileBtn;
+    }
+}
+
---
+++ Filhanterare/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Filhanterare
+{
+    public partial class Form1 : Form
+    {
+
+        private string NameOfTheCurrentText;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveBtn_Click(object sender, EventArgs e)
+        {
+            
+            string text = richTextBoxWindow.Text;
+            string path = "C:\\Users\\andyw\\Desktop\\NewFile.txt";
+            if (text != null && NameOfTheCurrentText != null)
+            {
+                System.IO.File.WriteAllText(NameOfTheCurrentText, text);
+            }
+            else
+            {
+
+            }
+            try
+            {
+                using (FileStream fs = File.Create(path))
+                {
+                    Byte[] info = new UTF8Encoding(true).GetBytes(text);
+                    // Add some information to the file.
+                    fs.Write(info, 0, info.Length);
+                }
+            }
+            catch (IOException ex)
+            {
+                throw ex;
+            }
+
+        }
+
+        private void SaveAsBtn_Click(object sender, EventArgs e)
+        {
+            // Displays a SaveFileDialog so the user can save the Image  
+            // assigned to Button2.  
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "Text|*.txt";
+            saveFileDialog1.Title = "Save an text File";
+            saveFileDialog1.ShowDialog();
+            string text = richTextBoxWindow.Text;
+
+            // If the file name is not an empty string open it for saving.  
+            if (saveFileDialog1.FileName != "")
+            {
+                // Saves the Image via a FileStream created by the OpenFile method.  
+                //System.IO.FileStream fs =
+                //   (System.IO.FileStream)saveFileDialog1.OpenFile();
+                // Saves the Image in the appropriate ImageFormat based upon the  
+                // File type selected in the dialog box.  
+                // NOTE that the FilterIndex property is one-based.  
+                switch (saveFileDialog1.FilterIndex)
+                {
+                    case 1:
+                 
+                    using (FileStream f = File.Create(saveFileDialog1.FileName.ToString()))
+                    {
+                    Byte[] info = new UTF8Encoding(true).GetBytes(text);
+                    // Add some information to the file.
+
+                    f.Write(info, 0, info.Length);
+                    f.Close();
+                    }
+                        break;
+                }
+            }
+        }
+
+        private void OpenFileBtn_Click(object sender, EventArgs e)
+        {
+            //if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            //{
+            //    System.IO.StreamReader sr = new
+            //       System.IO.StreamReader(openFileDialog1.FileName);
+            //    MessageBox.Show(sr.ReadToEnd());
+            //    sr.Close();
+            //}
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+
+                    //Read the contents of the file into a stream
+                    var fileStream = openFileDialog.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        fileContent = reader.ReadToEnd();
+                    }
+
+                    if(fileContent != null)
+                    {
+                        richTextBoxWindow.Text = fileContent.ToString();
+                    }
+                    NameOfTheCurrentText = filePath.ToString();
+                }
+            }
+        }
+    }
+}
